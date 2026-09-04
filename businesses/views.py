a4cd395048e0984c845be2ea58008647b67c15a9
maxwel07 <irungumaxwel7@gmail.com>
2026-09-04 15:58:51 +0300

    modified businesses app

--- businesses/views.py
+++ businesses/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def business(request):
+    return render(request, 'businesses.html')
--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import home
+from businesses.views import business
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name='home'),
+    path('businesses/', business, name='business_page')
 ]
